0aef6547129388f8af9a5db296d184d7749daec1
Keon Jin <dkjgoalie@gmail.com>
2021-09-18 17:24:21 -0400

    Add group counts endpoint

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const {Users} = require('../models/user.models')
+const { Users } = require('../models/user.models')
 
-router.post('/create', async (req, res) => {  
+router.post('/create', async (req, res) => {
   const user_id = req.body.userID
   const group_id = req.body.groupID
   const group_name = req.body.groupName
   const happy_count = req.body.happyCount
   const neutral_count = req.body.neutralCount
   const sad_count = req.body.sadCount
-  console.log(req)
-  try{
-    await Users.sync({force: false})
+  try {
+    await Users.sync({ force: false })
     await Users.create({
       "user_id": user_id,
       "group_id": group_id,
@@ -21,10 +20,23 @@ router.post('/create', async (req, res) => {
       "neutral_count": neutral_count,
       "sad_count": sad_count
     })
-  }catch(err){
-    res.status(400).json({"error": err})
+  } catch (err) {
+    res.status(400).json({ "error": err })
   }
-  res.status(200).json({"success": true})
+  res.status(200).json({ "success": true })
+})
+
+router.get('/group-counts', async (req, res) => {
+  const user_id = req.body.userID
+  try {
+    await Users.sync({ force: false })
+    const data = await Users.findAll({ where: { user_id: user_id } })
+    console.log(data)
+    res.status(200).json("")
+  } catch (err) {
+    res.status(400).json({ "error": err })
+  }
+
 })
 
 module.exports = router
