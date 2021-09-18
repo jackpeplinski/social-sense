c5040149a201d4ee7110c81c60a1b1df82fdc44b
Keon Jin <dkjgoalie@gmail.com>
2021-09-18 17:58:30 -0400

    Complete group counts endpoints

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,3 +1,4 @@
+const { raw } = require('body-parser');
 const express = require('express');
 const router = express.Router();
 
@@ -28,11 +29,22 @@ router.post('/create', async (req, res) => {
 
 router.get('/group-counts', async (req, res) => {
   const user_id = req.body.userID
+  let response = {}
   try {
     await Users.sync({ force: false })
     const data = await Users.findAll({ where: { user_id: user_id } })
-    console.log(data)
-    res.status(200).json()
+
+    data.forEach(group => {
+      raw_info = group.dataValues
+      response[raw_info.group_id] = {
+        "group_name": raw_info.group_name,
+        "happy_count": raw_info.happy_count,
+        "neutral_count": raw_info.neutral_count,
+        "sad_count": raw_info.sad_count
+      }
+    })
+
+    res.status(200).json(response)
   } catch (err) {
     res.status(400).json({ "error": err })
   }
