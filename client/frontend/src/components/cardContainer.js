07b6168298b744e36829f7e021dfa4c779e5a4c1
Keon Jin <dkjgoalie@gmail.com>
2021-09-18 23:23:21 -0400

    add card container

--- client/frontend/src/components/cardContainer.js
+++ client/frontend/src/components/cardContainer.js
@@ -1,7 +1,33 @@
 import React from "react";
 import Card from "./card";
+import {/*useState,*/ useEffect} from "react";
+const axios = require('axios')
 
 const CardContainer = () => {
+/**
+   * data = {group_id: {group_name, happy_count, neutral_count, sad_count}, group_id: {...}}
+   * number = data.length()
+   */
+  // eslint-disable-next-line
+ const fields = [];
+ useEffect(() => {
+  async function getGroupCards() {
+    try{
+      const data = await axios.get(`http://localhost:3000/api/users/group-counts`, {params: {"userID": 1}})
+      const numberOfCards = Object.keys(data).length
+      
+      
+      for (var i = 0; i < numberOfCards; i++) {
+        fields.push(<Card />);
+      }
+
+    }catch(err){
+
+    }
+  }
+  getGroupCards()
+}, [])
+
   return (
     <div className="card-container">
       <div className="card-container-label-wrapper">
@@ -12,6 +38,7 @@ const CardContainer = () => {
         </div>
       </div>
       <Card />
+      {fields}
     </div>
   );
 };
